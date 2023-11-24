9297c78351e91b94a83a9cab956785ede21fa16c
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2023-11-24 09:18:51 -0300

    feat: teste

--- math_test.go
+++ math_test.go
@@ -6,7 +6,7 @@ func TestSoma(t *testing.T) {
 
 	total := soma(20, 15)
 
-	if total != 30 {
+	if total != 35 {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 35)
 	}
 }
